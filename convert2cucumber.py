0bddf750781546b1048df6672a2985ed66869f26
Michael Ho <michael.ho@bluespurs.com>
2018-11-10 22:06:00 -0400

    adding convert behave json to cucumber format

---
+++ convert2cucumber.py
@@ -0,0 +1,11 @@
+import json
+import behave2cucumber
+import sys
+
+fPath=str(sys.argv[1])
+cucumber_json = None
+with open(fPath) as behave_json:
+    cucumber_json = behave2cucumber.convert(json.load(behave_json))
+
+with open(fPath, "w") as of:
+    json.dump(cucumber_json, of)
